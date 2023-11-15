d8d4bee6c0e45afcb7e9522fea82b56689e1d95b
Vumanh-mvp <vuducmanh10a@gmail.com>
2023-11-15 14:16:05 +0700

    Method Overloading

--- lab02/AimsProject/src/soict/Aims.java
+++ lab02/AimsProject/src/soict/Aims.java
@@ -23,5 +23,20 @@ public class Aims {
         anOrder.removeDigitalVideoDisc(dvd3);
         System.out.println("Total Cost is: ");
         System.out.println(anOrder.totalCost());
+
+        // Lab03: Check method overloading:
+        // Example using addDigitalVideoDisc(DigitalVideoDisc[] dvdList)
+        DigitalVideoDisc[] dvdArray = new DigitalVideoDisc[3];
+        dvdArray[0] = new DigitalVideoDisc("Movie 1", "Category 1", "Director 1", 120, 19.99f);
+        dvdArray[1] = new DigitalVideoDisc("Movie 2", "Category 2", "Director 2", 150, 24.99f);
+        dvdArray[2] = new DigitalVideoDisc("Movie 3", "Category 1", "Director 3", 110, 17.99f);
+
+        anOrder.addDigitalVideoDisc(dvdArray);
+
+        // Example using addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2)
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Movie A", "Category 2", "Director A", 130, 21.99f);
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Movie B", "Category 3", "Director B", 140, 22.99f);
+
+        anOrder.addDigitalVideoDisc(dvd4, dvd5);
     }
 }
